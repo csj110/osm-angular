e9ec1ddde49ba118fb0beb78530f816bc9913843
csj110 <32285817+csj110@users.noreply.github.com>
2019-07-02 22:02:26 +0800

    init

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400,
+    effect: 'cube',
+    height: 400,
+    duration:500,
+    loop: true
+  };
+  constructor() {
 
-  constructor() {}
+  }
+  ngOnInit() {
+
+  }
+  handleInit(e) {
+    e.target.startAutoplay();
+  }
 
 }
